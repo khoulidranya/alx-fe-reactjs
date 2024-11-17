ad459973a4cfdce1eefd85a5022010dc59c2742a
khoulidranya <khoulidranya@gmail.com>
2024-11-17 22:03:35 +0100

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
   return (
     <>
     <WelcomeMessage />
+    <Header />
+    <MainContent />
+    <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
