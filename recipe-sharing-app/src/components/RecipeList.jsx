8cf11c25c44b7b7f81526b027c6082bea8864494
khoulidranya <31227285+l4rb1@users.noreply.github.com>
2024-12-09 22:50:36 +0100

    Initial commit with Zustand store and basic components

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useRecipeStore } from '../store/recipeStore';
 
 const RecipeList = () => {
@@ -11,7 +12,9 @@ const RecipeList = () => {
       ) : (
         filteredRecipes.map((recipe) => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <Link to={`/recipes/${recipe.id}`}>
+              <h3>{recipe.title}</h3>
+            </Link>
             <p>{recipe.description}</p>
           </div>
         ))
