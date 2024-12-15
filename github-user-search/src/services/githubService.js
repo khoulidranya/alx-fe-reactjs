a580baeb3375aeab09bd85829406cd5e0d984cbc
khoulidranya <31227285+l4rb1@users.noreply.github.com>
2024-12-15 22:58:38 +0100

    components ffxjpvgg

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,26 @@
-// src/services/githubService.js
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
+// Function to fetch user data from GitHub with search parameters
+export const fetchUserData = async (username, location = '', minRepos = 0) => {
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
+    // Construct the search query with parameters
+    let query = `q=${username}`;
+    
+    if (location) {
+      query += `+location:${location}`;
+    }
+    
+    if (minRepos) {
+      query += `+repos:>=${minRepos}`;
+    }
+
+    // Make the API request to search for users based on the query
+    const response = await axios.get(`https://api.github.com/search/users?${query}`);
+
+    // Return the data from the response
+    return response.data.items; // This will be an array of user objects
   } catch (error) {
-    throw new Error("User not found");
+    // If there's an error, throw a specific message
+    throw new Error("Looks like we can't find the user");
   }
 };
