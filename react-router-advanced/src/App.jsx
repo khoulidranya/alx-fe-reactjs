5338c803e0209d19b2cba95ddc0a6f71303594f8
khoulidranya <31227285+l4rb1@users.noreply.github.com>
2024-12-15 20:31:19 +0100

    components ffcv

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,42 @@
-import ProtectedRoute from './components/ProtectedRoute';
-const isAuthenticated = true; // Simulated authentication
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ProfileDetails from "./pages/ProfileDetails";
+import ProfileSettings from "./pages/ProfileSettings";
+import BlogPost from "./pages/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NotFound from "./pages/NotFound";
 
-<Route
-  path="/profile/*"
-  element={
-    <ProtectedRoute isAuthenticated={isAuthenticated}>
-      <Profile />
-    </ProtectedRoute>
-  }
-/>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public Route */}
+        <Route path="/" element={<Home />} />
+
+        {/* Dynamic Route */}
+        <Route path="/blog/:postId" element={<BlogPost />} />
+
+        {/* Protected Route */}
+        <Route 
+          path="/profile/*" 
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          {/* Nested Routes within Profile */}
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+
+        {/* Catch-All Route */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
