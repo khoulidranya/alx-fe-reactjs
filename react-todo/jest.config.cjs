491982cf99139cb4da803c4ff4cfe3215a42d936
khoulidranya <khoulidranya@gmail.com>
2025-01-16 21:09:44 +0100

    testd

---
+++ react-todo/jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    transform: {
+      "^.+\\.[jt]sx?$": "babel-jest"
+    },
+    testEnvironment: "jsdom",
+    moduleFileExtensions: ["js", "jsx"],
+    moduleNameMapper: {
+      "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    }
+  };
+  
--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,31 +1,8 @@
-import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import TodoList from "../components/TodoList";
+import { render, screen } from '@testing-library/react';
+import TodoList from '../TodoList';
 
 test("renders initial todos", () => {
     render(<TodoList />);
     expect(screen.getByText("Learn React")).toBeInTheDocument();
     expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
-});
-
-test("adds a new todo", () => {
-    render(<TodoList />);
-    fireEvent.change(screen.getByPlaceholderText("Add a todo"), { target: { value: "New Todo" } });
-    fireEvent.click(screen.getByText("Add"));
-    expect(screen.getByText("New Todo")).toBeInTheDocument();
-});
-
-test("toggles todo completion", () => {
-    render(<TodoList />);
-    const todoItem = screen.getByText("Learn React");
-    fireEvent.click(todoItem);
-    expect(todoItem).toHaveStyle("text-decoration: line-through");
-});
-
-test("deletes a todo", () => {
-    render(<TodoList />);
-    const deleteButton = screen.getAllByText("Delete")[0];
-    fireEvent.click(deleteButton);
-    expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
 });
