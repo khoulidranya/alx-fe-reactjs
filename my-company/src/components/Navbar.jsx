5e4d2a59d95b8cba6b79d084274e0915005f58af
khoulidranya <khoulidranya@gmail.com>
2025-01-16 19:41:37 +0100

    testd

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,11 +3,27 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
-      <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
+    <nav
+      style={{
+        backgroundColor: "#f8f9fa", // Light background color
+        display: "flex",           // Flexbox for layout
+        justifyContent: "center",  // Center-align links
+        padding: "10px",           // Padding around the nav
+        borderBottom: "1px solid #ccc" // Bottom border
+      }}
+    >
+      <Link to="/" style={{ margin: "0 15px", textDecoration: "none" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ margin: "0 15px", textDecoration: "none" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ margin: "0 15px", textDecoration: "none" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ margin: "0 15px", textDecoration: "none" }}>
+        Contact
+      </Link>
     </nav>
   );
 }
